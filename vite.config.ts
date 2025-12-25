794ca9eb9f9b3cf3be6db830695e5bea850e0c4e
Lina Chatelin <linactl@air-de-lina-2.home>
2025-12-25 16:36:58 +0100

    prepare for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // 👇 OBLIGATOIRE pour GitHub Pages
-  base: "/drive.prime/",
+  base: "/",
 
   server: {
     host: "",
